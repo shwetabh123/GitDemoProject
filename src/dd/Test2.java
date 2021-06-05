5f1a178d391b9a5df097b5323886c2e8a4c33339
shwetabh <shwetabh.srivastava@gmail.com>
2021-06-05 19:53:21 +0530

    commiting test2 to branch-tester

---
+++ src/dd/Test2.java
@@ -0,0 +1,19 @@
+package dd;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		
+		String hungry ="yes";
+		
+		if(hungry =="yes")
+			System.out.println("eat somethings...");
+		else
+		{
+			System.out.println("Do work..");
+			
+		}
+		
+	}
+	
+}
